f3dcddaa2703ff286ad3cbd88ddfae57c6165fda
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2025-08-14 00:36:39 -0700

    Update speeds.py

--- ros2/src/auv/logic/speeds.py
+++ ros2/src/auv/logic/speeds.py
@@ -1,49 +1,38 @@
 def hover():
 	print("hovering")
-	A1.setSpeed(95)
-	A4.setSpeed(95)
-	M1.setSpeed(95)
-	M4.setSpeed(95)
+	A2.setSpeed(95)
+	A3.setSpeed(95)
+	M2.setSpeed(95)
+	M3.setSpeed(95)
+	A2.run()
+	A3.run()
+	M2.run()
+	M3.run()
+
+def right():
+	print("turning right")
+	A1.setSpeed(100)
+	A4.setSpeed(100)
+	M1.setSpeed(80)
+	M4.setSpeed(80)
+	A1.run()
+	A4.run()
+	M1.run()
+	M4.run()
+	
+def left():
+	print("turning left")
+	A1.setSpeed(80)
+	A4.setSpeed(80)
+	M1.setSpeed(100)
+	M4.setSpeed(100)
 	A1.run()
 	A4.run()
 	M1.run()
 	M4.run()
 
-def right():
-	print("turning right")
-	A2.setSpeed(100)
-	A3.setSpeed(100)
-	M2.setSpeed(80)
-	M3.setSpeed(80)
-	A2.run()
-	A3.run()
-	M2.run()
-	M3.run()
-	
-def left():
-	print("turning left")
-	A2.setSpeed(80)
-	A3.setSpeed(80)
-	M2.setSpeed(100)
-	M3.setSpeed(100)
-	A2.run()
-	A3.run()
-	M2.run()
-	M3.run()
-
 def forward():
 	print("going forward")
-	A2.setSpeed(100)
-	A3.setSpeed(100)
-	M2.setSpeed(100)
-	M3.setSpeed(100)
-	A2.run()
-	A3.run()
-	M2.run()
-	M3.run()
-
-def down():
-	print("going down")
 	A1.setSpeed(100)
 	A4.setSpeed(100)
 	M1.setSpeed(100)
@@ -52,6 +41,17 @@ def down():
 	A4.run()
 	M1.run()
 	M4.run()
+
+def down():
+	print("going down")
+	A2.setSpeed(100)
+	A3.setSpeed(100)
+	M2.setSpeed(100)
+	M3.setSpeed(100)
+	A2.run()
+	A3.run()
+	M2.run()
+	M3.run()
 
 def up():
 	print("going up")
