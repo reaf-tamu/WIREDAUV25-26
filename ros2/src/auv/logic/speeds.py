f1cf0e48f63c035adf386f3fc7687c980e20f8f2
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2025-08-14 00:32:27 -0700

    Create speeds.py

---
+++ ros2/src/auv/logic/speeds.py
@@ -0,0 +1,57 @@
+def hover():
+	print("hovering")
+	A1.setSpeed(95)
+	A4.setSpeed(95)
+	M1.setSpeed(95)
+	M4.setSpeed(95)
+	A1.run()
+	A4.run()
+	M1.run()
+	M4.run()
+
+def right():
+	print("turning right")
+	A2.setSpeed(100)
+	A3.setSpeed(100)
+	M2.setSpeed(80)
+	M3.setSpeed(80)
+	A2.run()
+	A3.run()
+	M2.run()
+	M3.run()
+	
+def left():
+	print("turning left")
+	A2.setSpeed(80)
+	A3.setSpeed(80)
+	M2.setSpeed(100)
+	M3.setSpeed(100)
+	A2.run()
+	A3.run()
+	M2.run()
+	M3.run()
+
+def forward():
+	print("going forward")
+	A2.setSpeed(100)
+	A3.setSpeed(100)
+	M2.setSpeed(100)
+	M3.setSpeed(100)
+	A2.run()
+	A3.run()
+	M2.run()
+	M3.run()
+
+def down():
+	print("going down")
+	A1.setSpeed(100)
+	A4.setSpeed(100)
+	M1.setSpeed(100)
+	M4.setSpeed(100)
+	A1.run()
+	A4.run()
+	M1.run()
+	M4.run()
+
+def up():
+	print("going up")
