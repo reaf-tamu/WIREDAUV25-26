e4d9296cb8070be9532fdfa3830c795858be5813
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2025-08-14 10:31:57 -0500

    Update ping_log.py

--- ros2/src/auv/logic/ping_log.py
+++ ros2/src/auv/logic/ping_log.py
@@ -4,7 +4,7 @@ from brping import Ping1D
 # maybe check conifidence later
 def check_depth(goal, bound, depth):
     if depth > (goal + bound):
-        print("D")
+        return "D"
     if depth < (goal- bound):
-        print("U")
+        return "U"
     return
