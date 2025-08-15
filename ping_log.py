c40034c0c5a047cd37a5e17dc32f84ea41c2213c
reaf-tamu <35748267+reaf-tamu@users.noreply.github.com>
2025-08-15 04:29:27 -0700

    Create ping_log.py

---
+++ ping_log.py
@@ -0,0 +1,10 @@
+import time
+from brping import Ping1D
+
+# maybe check conifidence later
+def check_depth(goal, bound, depth):
+    if depth > (goal + bound):
+        return "D"
+    if depth < (goal- bound):
+        return "U"
+    return
